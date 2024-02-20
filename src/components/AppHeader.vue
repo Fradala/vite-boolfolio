221332e183f220faf16b9db36a1ad7a063f0bdec
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-02-20 15:33:19 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <header>
+        <div>
+            header
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,49 @@
+<template lang="">
+    <main class="container">
+        <section class=row>
+            <div class="col-12">
+                <h1>
+                    I miei progetti:
+                </h1>
+            </div>
+
+        </section>
+        
+    </main>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'AppMain',
+
+    data() {
+
+    },
+
+    methods:{
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                
+                }
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.warn(error);
+            })
+            
+        }
+    },
+
+    created() {
+        this.getProjects();
+    },
+    
+}
+</script>
+<style lang="">
+    
+</style>
